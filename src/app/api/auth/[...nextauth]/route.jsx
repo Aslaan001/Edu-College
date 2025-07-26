f89e32a2908d354e558d2859122099535cbcec35
Aslaan001 <aslaankhan640@gmail.com>
2025-07-26 15:52:11 +0530

    🚀 Added full Edu-College project code

--- src/app/api/auth/[...nextauth]/route.jsx
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -6,8 +6,8 @@ import GoogleProvider from "next-auth/providers/google"
 const handler =NextAuth({
     providers: [
         GoogleProvider({
-          clientId:"219008763940-htlv0h0sb9o80rmafl26lkbviq5863lf.apps.googleusercontent.com",
-          clientSecret:"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",
+          clientId:process.env.OAUTH_ID,
+          clientSecret:process.env.OAUTH_KEY,
         })
       ]
 });
--- src/hook/useGetResponse.js
+++ src/hook/useGetResponse.js
@@ -1,9 +1,11 @@
-import {GoogleGenerativeAI} from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyCVItCgBsEj6jWlOP20LMWbb0WBXt4i6ms");
+// Securely load the Gemini key from the environment
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const getGeminiResponse = async (prompt) => {
+  console.log("Gemini Key from env:", process.env.GEMINI_KEY);
   const result = await model.generateContent(prompt);
   return result.response.text();
 };
