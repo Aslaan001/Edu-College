48f0ac6e345e7e966965c970aa9a3d9972d04e26
Aslaan001 <aslaankhan640@gmail.com>
2025-08-14 15:51:52 +0530

    Fix self-ping: use require('node-fetch') and only run on server

--- src/lib/selfPing.js
+++ src/lib/selfPing.js
@@ -1,9 +1,11 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+if (typeof window === "undefined") {
+  const fetch = require('node-fetch');
 
-const SELF_URL = "https://edu-college.onrender.com/";
+  const SELF_URL = "https://edu-college.onrender.com/";
 
-setInterval(() => {
-  fetch(SELF_URL)
-    .then(res => console.log(`[Self-Ping] Pinged ${SELF_URL} at ${new Date().toISOString()}`))
-    .catch(err => console.error('[Self-Ping] Error:', err));
-}, 10 * 60 * 1000); // every 10 minutes
+  setInterval(() => {
+    fetch(SELF_URL)
+      .then(res => console.log(`[Self-Ping] Pinged ${SELF_URL} at ${new Date().toISOString()}`))
+      .catch(err => console.error('[Self-Ping] Error:', err));
+  }, 10 * 60 * 1000); // every 10 minutes
+}
